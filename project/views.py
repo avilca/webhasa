9e23104185d0bcc9eaa2e3eb2b9d8ac4d38ca206
avilca <avilca85@gmail.com>
2025-03-05 12:34:52 -0500

    Paginador en proyectos

--- project/views.py
+++ project/views.py
@@ -1,9 +1,15 @@
 from django.shortcuts import render
+from django.core.paginator import Paginator
 from .models import Project
 
 # Create your views here.
 def proyectos(request):
-    projects = Project.objects.all()
+    projects = Project.objects.order_by('-created')
+
+    #Paginador de 6 en 6
+    paginator = Paginator(projects, 6)
+    page_number = request.GET.get('page', 1)
+    projects = paginator.page(page_number)
 
     return render(request, 'project/proyectos.html',
                   {'projects': projects})
