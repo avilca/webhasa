bab683e2284e869b236d51b3ed58ab2e54d3ab10
avilca <avilca85@gmail.com>
2025-04-21 12:57:53 -0500

    cambias enlaces(link) de redes sociales

--- social/admin.py
+++ social/admin.py
@@ -5,4 +5,11 @@ from .models import Link
 class LinkAdmin(admin.ModelAdmin):
     readonly_fields = ('created', 'updated')
 
+    #para poner en modificar los enlaces de la redes sociales
+    def get_readonly_fields(self, request, obj = None):
+        if request.user.groups.filter(name="Hasa").exists():
+            return ('created', 'updated', 'key', 'name')
+        else:
+            return ('created', 'updated')
+
 admin.site.register(Link, LinkAdmin)    
